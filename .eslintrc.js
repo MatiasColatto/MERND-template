cceb930822f71d1cb4043d781d56a18aeb0f8543
MatiasColatto <mimaco1234@gmail.com>
2023-11-02 19:11:41 -0300

    adding actions

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+      "max-len": ["error", {
+        "code": 50,  
+        "ignoreUrls": true, 
+        "ignoreStrings": true, 
+        "ignoreTemplateLiterals": true, 
+      }],
+    },
+  };
+  
